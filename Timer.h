848bed49225410bfe6ea8402cd6927e8905056ec
bravehart <claudia_rafaela.rogoz@cti.pub.ro>
2014-03-11 21:49:59 +0200

    Create Timer.h

---
+++ Timer.h
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include <sys/timeb.h>
+
+class Timer{
+	
+	int startTime, stopTime, currentTime;
+	
+	struct timeb startSysTime, stopSysTime, currentSysTime;
+	bool isRunning;
+	
+	Timer();
+	void startTimer();
+	void stopTimer();
+	void resetTimer();
+	void displayTime();
+	
+
+};
