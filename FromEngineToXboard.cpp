50f1d199cb0498c14eabf6749f96471f1edbaa7d
JenLawrence <diana_despa@yahoo.com>
2014-03-12 15:46:50 +0200

    11/03/2014

---
+++ FromEngineToXboard.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include "FromEnginetoXboard.h"
+	
+int FromEnginetoXboard::parseMove(char* move){
+
+	fputs("b2b3\n",stdout);
+	return 0;
+}
+
---
+++ FromEnginetoXboard.h
@@ -0,0 +1,10 @@
+
+#ifndef _TOXBOARD_H
+#define _TOXBOARD_H
+
+class FromEnginetoXboard{
+	public: 
+	int parseMove(char* move);
+
+};
+#endif
