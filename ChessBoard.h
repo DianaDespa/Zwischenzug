2a14c1462e08584995cc3c28bbc1a1d14eafd258
ahtralabah <alexandru_mihail.orlando@cti.pub.ro>
2014-03-26 23:26:49 +0200

    Am modificat putin ca sa pot testa.
    O sa revina inapoi private la final

--- ChessBoard.h
+++ ChessBoard.h
@@ -8,6 +8,7 @@
 #include <vector>
 #include <ctime>
 #include <stdlib.h>
+#include <cmath>
 #include "constants.h"
 
 // Class that implements the chess board's current state and functions for
@@ -15,27 +16,31 @@
 class ChessBoard {
 private:
 	static std::string ALPHA_NUMERIC_POSITIONS[64];
-	static board* table;
 	static int initial_position, final_position;
 	static bool generateValidPawnMove(int pos, bool isWhite);
 	static bool generateValidPawnAttack(int pos, bool isWhite);
 	static bool randomPositionPawn(bool isWhite);
-	
 	static bool kingInDanger(bool isWhite);
 	
+	static bool randomPositionKnight(bool isWhite);
 
 public:
 	ChessBoard(void);
 	~ChessBoard(void);
 	static std::string convertToBitString(long long value);
 	static BITBOARD getAttacks(int pos);
-	
+
 	static void updateOpponentMove(char* positions);
 	static void initializeBitboard(void);
 	static void movePiece(int initial_pos, int final_pos);
 	static bool randomPiece(bool isWhite);
 	static std::string finalPosFunc(void);
 	static std::string initialPosFunc(void);
+	
+	//MUTA
+	static board* table;
+	static bool generateValidKnightMove(int pos, bool isWhite);
+	static bool generateValidBishopMove(int pos, bool isWhite);
 };
 
 #endif
